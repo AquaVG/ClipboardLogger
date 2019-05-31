16aecc1d508c4a748d9cade25ed1d89900e0faf4
Влад Головатюк <vladd.golovatyuk@gmail.com>
2019-05-31 14:40:53 +0300

    Add files via upload

---
+++ ClipboardLogger/ClipboardLogger/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ClipboardLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.permanNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.processNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.открытьПапкуСЗаписямиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьЗаписиЗаСегодняToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.остановитьРаботуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closingNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // permanNotify
+            // 
+            this.permanNotify.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.permanNotify.BalloonTipText = "Clipboard Logger has been launched successfully.";
+            this.permanNotify.BalloonTipTitle = "Clipboard Logger";
+            this.permanNotify.Icon = ((System.Drawing.Icon)(resources.GetObject("permanNotify.Icon")));
+            this.permanNotify.Text = "Clipboard Logger";
+            this.permanNotify.BalloonTipClicked += new System.EventHandler(this.NotifyIcon_Hide);
+            this.permanNotify.BalloonTipClosed += new System.EventHandler(this.NotifyIcon_Hide);
+            this.permanNotify.Click += new System.EventHandler(this.NotifyIcon_Hide);
+            // 
+            // processNotify
+            // 
+            this.processNotify.ContextMenuStrip = this.contextMenuStrip1;
+            this.processNotify.Icon = ((System.Drawing.Icon)(resources.GetObject("processNotify.Icon")));
+            this.processNotify.Text = "Clipboard Logger";
+            this.processNotify.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьПапкуСЗаписямиToolStripMenuItem,
+            this.открытьЗаписиЗаСегодняToolStripMenuItem,
+            this.остановитьРаботуToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(223, 70);
+            // 
+            // открытьПапкуСЗаписямиToolStripMenuItem
+            // 
+            this.открытьПапкуСЗаписямиToolStripMenuItem.Name = "открытьПапкуСЗаписямиToolStripMenuItem";
+            this.открытьПапкуСЗаписямиToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.открытьПапкуСЗаписямиToolStripMenuItem.Text = "Открыть папку с записями";
+            this.открытьПапкуСЗаписямиToolStripMenuItem.Click += new System.EventHandler(this.OpenFolder);
+            // 
+            // открытьЗаписиЗаСегодняToolStripMenuItem
+            // 
+            this.открытьЗаписиЗаСегодняToolStripMenuItem.Name = "открытьЗаписиЗаСегодняToolStripMenuItem";
+            this.открытьЗаписиЗаСегодняToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.открытьЗаписиЗаСегодняToolStripMenuItem.Text = "Открыть записи за сегодня";
+            this.открытьЗаписиЗаСегодняToolStripMenuItem.Click += new System.EventHandler(this.OpenToday);
+            // 
+            // остановитьРаботуToolStripMenuItem
+            // 
+            this.остановитьРаботуToolStripMenuItem.Name = "остановитьРаботуToolStripMenuItem";
+            this.остановитьРаботуToolStripMenuItem.Size = new System.Drawing.Size(222, 22);
+            this.остановитьРаботуToolStripMenuItem.Text = "Остановить работу";
+            this.остановитьРаботуToolStripMenuItem.Click += new System.EventHandler(this.StopApplication);
+            // 
+            // closingNotify
+            // 
+            this.closingNotify.BalloonTipText = "Clipboard Logger has been closed. Click to launch it again.";
+            this.closingNotify.BalloonTipTitle = "Clipboard Logger";
+            this.closingNotify.Icon = ((System.Drawing.Icon)(resources.GetObject("closingNotify.Icon")));
+            this.closingNotify.Text = "Clipboard Logger";
+            this.closingNotify.BalloonTipClicked += new System.EventHandler(this.ClosingNotify_BalloonTipClicked);
+            this.closingNotify.BalloonTipClosed += new System.EventHandler(this.ClosingNotify_BalloonTipClosed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon permanNotify;
+        private System.Windows.Forms.NotifyIcon processNotify;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьПапкуСЗаписямиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьЗаписиЗаСегодняToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem остановитьРаботуToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon closingNotify;
+    }
+}
+
---
+++ ClipboardLogger/ClipboardLogger/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+namespace ClipboardLogger
+{
+    public partial class Form1 : Form
+    {
+        string today;
+        [DllImport("User32.dll")]
+        protected static extern int SetClipboardViewer(int hWndNewViewer);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove,
+                                    IntPtr hWndNewNext);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg,
+                                             IntPtr wParam,
+                                             IntPtr lParam);
+
+        IntPtr nextClipboardViewer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Opacity = 0;
+            ShowInTaskbar = false;
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int)
+                         this.Handle);
+            permanNotify.Visible = true;
+            permanNotify.ShowBalloonTip(10);
+            SetAutorunValue(true);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            // defined in winuser.h
+            const int WM_DRAWCLIPBOARD = 0x308;
+            const int WM_CHANGECBCHAIN = 0x030D;
+
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    DisplayClipboardData();
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam,
+                                  m.LParam);
+                    break;
+
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == nextClipboardViewer)
+                        nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam,
+                                         m.LParam);
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        const string name = "Clipboard Logger";
+        public bool SetAutorunValue(bool autorun)
+        {
+            string ExePath = Application.ExecutablePath;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autorun)
+                    reg.SetValue(name, ExePath);
+                else
+                    reg.DeleteValue(name);
+
+                reg.Close();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        private void DisplayClipboardData()
+        {
+            today = DateTime.Today.ToShortDateString();
+            File.AppendAllText("Log" + @"\" + today + ".txt", Clipboard.GetText() + Environment.NewLine + Environment.NewLine);
+            if (new DirectoryInfo(@"Log\").GetFiles().Length >= 30)
+            {
+                new DirectoryInfo(@"Log\").GetFileSystemInfos()
+                                                                                    .OrderByDescending(fi => fi.CreationTime)
+                                                                                    .Last()
+                                                                                    .Delete();
+            }
+        }
+
+        private void NotifyIcon_Hide(object sender, EventArgs e)
+        {
+            (sender as NotifyIcon).Visible = false;
+        }
+
+        private void OpenFolder(object sender, EventArgs e)
+        {
+            Process.Start("Log\\");
+        }
+
+        private void OpenToday(object sender, EventArgs e)
+        {
+            Process.Start("Log\\" + today + ".txt");
+        }
+
+        private void StopApplication(object sender, EventArgs e)
+        {
+            closingNotify.Visible = true;
+            closingNotify.ShowBalloonTip(10);
+        }
+
+        private void ClosingNotify_BalloonTipClicked(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void ClosingNotify_BalloonTipClosed(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
